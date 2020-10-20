f8638a27c044d60bb2c2ba5d3a72fc6a93dc1891
Carlos Vicient <carlosvm@Carloss-MacBook-Pro.local>
2020-10-20 23:19:05 +0200

    Adding lamda slack and form

--- src/App.js
+++ src/App.js
@@ -31,6 +31,48 @@ class LambdaDemo extends Component {
   }
 }
 
+class SlackMessage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: false, text: null, error: null, success: false };
+  }
+  handleText = (e) => {
+    this.setState({ text: e.target.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ loading: true });
+    fetch('/.netlify/functions/slack', {
+      method: "POST",
+      body: JSON.stringify({
+        text: this.state.text
+      })
+    })
+      .then(response => {
+        if (!response.ok) {
+          return response.text().then(err => { throw (err) });
+        }
+      })
+      .then(() => this.setState({ loading: false, text: null, success: true, error: null }))
+      .catch(err => this.setState({ loading: false, success: false, error: err.toString() }))
+  }
+  render() {
+    const { loading, text, error, success } = this.state;
+    return <form onSubmit={this.handleSubmit}>
+      {error && <p><strong>Error sending message: {error}</strong></p>}
+      {success && <p><strong>Done! Message sent to Slack</strong></p>}
+      <p>
+        <label>Your Message: <br />
+          <textarea onChange={this.handleText} value={text}></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit" disabled={loading}>{loading ? "Sending Slack Message..." : "Send a Slack Message"}</button>
+      </p>
+    </form>;
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -41,10 +83,13 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <LambdaDemo />
+          <SlackMessage/>
         </header>
       </div>
     )
   }
 }
+
+
 
 export default App
